b8ac60a3b05ac0325fc9594ff35f4ac9514a8d1f
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-06-19 16:45:23 +0530

    done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1001
+let accountEmail = "pankaj121@gmail.com"
+var accountPassword = "1212"
+accountCity = "Ahmedabad"
+accountEmail = "omjai@yahoo.com"
+accountPassword = "2020"
+accountCity = "Newyork"
+let accountState;
+
+//accountId = 2
+console.log(accountId);
+console.table([accountEmail, accountCity ,accountPassword])
+console.log(accountState);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version 
+//alert( 3 + 3) // we are using nodejs not brower
+console.log("runner");
+console.log(3 + 3) // code readability should be high
+//Data type
+
+let name = "pankaj"
+let Age = 24
+let LoggedIn = true
+
+// let Bigint 
+// string = ""
+// boolean = true/false
+// null standalone value
+// undefine =>
+    // symbol = unique primitive value
+
+// non primitive datatype
+
+//oject
+console.log(typeof true);
+console.log(typeof null); // tyep object
+console.log(typeof undefine); //type undefine
