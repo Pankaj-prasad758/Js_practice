2f96c91dc1ede5c3e1698f00777d7f5f7e2797d6
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-06-20 13:50:05 +0530

    done

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,47 @@
+let score = null
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "" => 0; false
+// "pankaj" => true
+
+let someNumber = 40
+ let stringNumber = String(someNumber)
+ //console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+ //******************** operations ************************* */
+
+ let value = 3
+ let negValue = -value
+ //console.log(negValue);
+// console.log(2 + 2);
+// console.log(2**3); //power
+ //console.log(2/2); //divide
+ //console.log(2%2); //reminder
+
+ str1 = "goodMorning"
+ str2 = " everyone"
+ str3 = str1 + str2
+//  console.log(str3);
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 200
+gameCounter++;
+// console.log(gameCounter);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
