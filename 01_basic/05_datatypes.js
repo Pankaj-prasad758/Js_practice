8abfb2094cd22e4b87236213fe8327f66614e823
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-06-20 16:17:06 +0530

    done

---
+++ 01_basic/05_datatypes.js
@@ -0,0 +1,36 @@
+// Primitive datatypes
+// 1)String 2)Boolean 3)Number 4)Null 5)Undefine 6)BigInt 7)Symbol
+ 
+const score = 90
+const scoreValue = 90.9
+const outSideTemp = null
+const userLoggedIn = false
+const userName = "Shadow"
+const userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id === anotherId);
+const bigNumber =78998789982932761515151515111515n
+//console.log(bigNumber);
+
+const exArray = ["hunt","hide","hussle","hunger"]
+console.log(typeof exArray);
+const productDetials = {
+    name:"hangover",
+    age:34,
+    place:"Mexico"
+}
+//console.log(productDetials);
+
+let myFunction = function(){
+    console.log("hello planet earth");
+   
+}
+//console.log(myFunction());
+
+// Non Primitive datatypes or Reference
+//Array
+//Object
+//Functions
+
