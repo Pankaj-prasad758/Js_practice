e154ebdb664743b63d6915d49845b8ee8167df07
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-06-25 20:37:47 +0530

    done

---
+++ 03_basic/01_function.js
@@ -0,0 +1,28 @@
+//Function
+
+//**function syntax */
+
+function myName(){
+    console.log("pankaj");
+}
+
+myName()
+
+function addTwoNumbers(number1 , number2){
+//   console.log(number1 + number2);
+// let addition = number1 + number2
+// return addition
+
+//**another way  */
+return number1 - number2
+console.log("pankaj"); // never execute beacuse of return
+
+}
+
+const addition = addTwoNumbers(22,8) 
+console.log("Addition: ", addition)
+
+function userLoggedIn(username){
+    return`${username} just logged in `
+}
+console.log(userLoggedIn("pankaj"))
