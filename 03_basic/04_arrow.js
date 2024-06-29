67f959c4d7474f94edef2e462b434b63e89524d6
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2024-06-29 15:23:30 +0530

    done

--- 03_basic/04_arrow.js
+++ 03_basic/04_arrow.js
@@ -1,17 +1,17 @@
 //** Arrow function */
 
-const user = {
-    name:"pankaj",
-    mobileNumber: 5556667771,
-    getMessage : function(){
-        // console.log(`${this.name}, welcome back on sunrise resort`);
-        // console.log(this);
-    }
-}
+// // const user = {
+//     name:"pankaj",
+//     mobileNumber: 5556667771,
+//     getMessage : function(){
+//         // console.log(`${this.name}, welcome back on sunrise resort`);
+//         // console.log(this);
+//     }
+// }
 // user.getMessage()
-user.name = "harsh"
+// user.name = "harsh"
 // user.getMessage()
-console.log(this);
+// console.log(this);
 
 function checkThis(){
     // console.log(this);
@@ -20,7 +20,7 @@ function checkThis(){
 
 function run() {
     let gun = "M16"
-    // console.log(this.gun);
+    console.log(this.gun);
 }
 run()  // **Output undefine,  beacuse this key word works only in object not in function
 const rifle = function () {
@@ -41,11 +41,58 @@ rifleNew()  // output undefine
  const addVal = (num1 , num2) => { // must use Return key word
     return num1 + num2
  }
- console.log(addVal(19 ,1));
+//  console.log(addVal(19 ,1));
 
  //**Impicit return */
 
  const addNum = (num1, num2) => num1 + num2 // no need to add return key word or use curly bracket
  const addNum2 = (num1, num2) => (num1 + num2) // why we use parenthesis becuase without parenthesis we cannot access object in Impicit return
- console.log(addNum(9,19));
- console.log(addNum2(9,19));
+//  console.log(addNum(9,19));
+//  console.log(addNum2(9,19));
+ const drink = () => {
+    let coofee = 100
+    console.log(this);
+ }
+//  drink() // output gives empty object
+const userPassword = function (){
+console.log( this);
+}
+// console.log(userPassword());
+
+// const userHistory = function(){
+//    var user = 10
+//    console.log(user);
+ 
+// }
+// console.log(userHistory());
+//  var user = 20
+ 
+const pan = "pankaj"
+let aka = "shadow"
+var world = "alone King"
+
+// console.table([pan, aka,world])
+let b = 1
+let a = ++b // prefix
+// console.log(a);
+let c = b++
+// console.log(c);
+
+const dummy = new String("funday never exist")
+// console.log(dummy.includes("fun"));
+// console.log(dummy.replace("fun","boring "));
+// console.log(dummy.substring(0,4)); // last val not count (4)
+// console.log(dummy.split()); // convert string into array
+// console.log(dummy.slice(0,4));
+
+
+const arra = [-1997.90990, 2000,30000]
+// console.log(birthday.toFixed(2));
+// console.log(birthday.toPrecision(4));
+// console.log(Math.max(...arra));
+// console.log(Math.floor(...arra));
+// console.log(Math.floor(...arra));
+// console.log(Math.random(...arra ) * 10 + 10);
+console.log(Math.abs(...arra ));
+console.log(Math.min(...arra ));
+
--- 03_basic/iife.js
+++ 03_basic/iife.js
@@ -1,15 +1,15 @@
 //**Immediately Invoked Function Expressions (IIFE) */
 
 (function garamMasala(){
-  console.log(`john and akshay`);
+  // console.log(`john and akshay`);
 })();
 
 (function(){ //Nmaed iife
-    console.log("pankaj");
+    // console.log("pankaj");
 })();  // ** remember when we use iife we must carefull about (;) after the execution
 
 ((name,price) => { //UnNamed iife
-    console.log(`${name} and ${price}`); //*8 how to pass variables in iife syntax
+    // console.log(`${name} and ${price}`); //*8 how to pass variables in iife syntax
 })("khali",999);
 //** */ IIFE (Immediately Invoked Function Expression): A function that runs as soon as it is defined.
 // *8Purpose: To avoid polluting the global scope and to create a local scope for variables and functions.
@@ -18,13 +18,29 @@
 
 (function username(customer,order){
     var password = "123asd"
-      console.log(`customer password ${password} customer name ${customer} order ${order}`);
+      // console.log(`customer password ${password} customer name ${customer} order ${order}`);
 })("mukund",100)
 // console.log(username());
 
-
-
-
-
-
 // console.log(garamMasala());
+
+
+// function funcode(){
+//   const userNick = "putiyaa"
+//   function boring(){
+//     const getUser = " pushkar"
+//     console.log(userNick + getUser);
+//   }
+//   boring()
+// // console.log(getUser);
+// }
+// funcode()
+// // console.log(userNick);
+
+// const userName = function(){
+//   const firstname = "pankaj"
+//   return firstname
+// }
+// console.log(userName());
+
+
